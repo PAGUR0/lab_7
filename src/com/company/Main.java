02ef3379df1ee725a098ef09d8686e28ad7ede96
Paguro <ivankosaev2006@gmail.com>
2023-03-03 12:59:10 +0300

    2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,14 +6,17 @@ class Calculator{
     double a;
     double b;
     double x;
+
     Calculator(double [] array){
         this.a = array[0];
         this.b = array[1];
         this.x = array[2];
     }
+
     double formula_1(){
         return 3 * x + 5;
     }
+
     double formula_2(){
         if ((a - b) != 0) {
             return (a - b) / (a + b);
@@ -21,12 +24,13 @@ class Calculator{
             return -1;
         }
     }
+
     double formula_3(){
         if (b != 0){
             double temp = a * x / b;
             if(temp % 1 == 0) {
                 int y = 1;
-                for (int i = 1; i <= a * x / b; i++) {
+                for (int i = 1; i <= temp; i++) {
                     y = y * i;
                 }
                 return y;
@@ -46,18 +50,23 @@ class Calculator{
 public class Main {
     public static double[] filler(){
         Scanner in = new Scanner(System.in);
+
         double[] result = new double[3];
+
         for (int i = 0; i < 3; i++) {
             result[i] = in.nextDouble();
         }
+
         return result;
     }
 
     public static void main(String[] args) {
+
         Calculator calc1 = new Calculator(filler());
         Calculator calc2 = new Calculator(filler());
         Calculator calc3 = new Calculator(filler());
         Calculator[] arr = {calc1, calc2, calc3};
+
         for(Calculator i:arr){
             System.out.println(i.formula_1());
             System.out.println(i.formula_2());
